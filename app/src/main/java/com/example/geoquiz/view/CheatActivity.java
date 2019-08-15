06ac6612acfac11247ed429699c6f04b09067fb6
VladisP <vlad19041999@yandex.ru>
2019-08-15 13:58:27 +0500

    Добавление активити с подсказкой.

---
+++ app/src/main/java/com/example/geoquiz/view/CheatActivity.java
@@ -0,0 +1,16 @@
+package com.example.geoquiz.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.geoquiz.R;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+    }
+}
